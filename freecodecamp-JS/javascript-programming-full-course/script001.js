1eca8d2297f0dd1844044825a85977c2c3096b8d
dudamdev <111284160+dudamdev@users.noreply.github.com>
2023-02-12 08:31:37 -0300

    update

--- freecodecamp-JS/javascript-programming-full-course/script001.js
+++ freecodecamp-JS/javascript-programming-full-course/script001.js
@@ -817,3 +817,78 @@
 // console.log(castle.features)
 
 // ##########################################################################################################################
+
+// let person = {
+//     name: "Eduarda",
+//     age: 22,
+//     country: "Brazil"
+// }
+
+// function logData() {
+//     return person.name + " is " + person.age + " years old and lives in " + person.country
+// }
+
+// console.log(logData())
+
+// ##########################################################################################################################
+
+// let age = 67
+
+// if (age < 6) {
+//     console.log("Free")
+// } else if (age < 18) {
+//     console.log("Child discount")
+// } else if (age < 27) {
+//     console.log("Student discount")
+// } else if (age < 67) {
+//     console.log("Full price")
+// } else {
+//     console.log("Senior citizen discount")
+// }
+
+// ##########################################################################################################################
+
+// let largeCountries = ["China", "India", "USA", "Indonesia", "Pakistan"]
+
+// console.log("The 5 largest countries in the world:")
+// for (let i = 0; i < largeCountries.length; i++) {
+//     console.log("- "+ largeCountries[i])
+// }
+
+// ##########################################################################################################################
+
+// let largeCountries = ["Tuvalu", "India", "USA", "Indonesia", "Monaco"]
+
+// largeCountries.pop()
+// largeCountries.push("Pakistan")
+// largeCountries.shift()
+// largeCountries.unshift("China")
+// console.log(largeCountries)
+
+// ##########################################################################################################################
+
+// let dayOfMonth = 13
+// let weekday = "Friday"
+
+// if (dayOfMonth === 13 && weekday === "Friday") {
+//     console.log("😱")
+// }
+
+// ##########################################################################################################################
+
+let hands = ["rock", "paper", "scissor"]
+
+// function random() {
+//     console.log(hands[Math.floor(Math.random()*3)])
+// }
+
+// random() --> MY SOLUTION
+
+function getHand() {
+    let randomIndex = Math.floor(Math.random() * 3)
+    return hands[randomIndex]
+}
+
+console.log(getHand())
+
+
