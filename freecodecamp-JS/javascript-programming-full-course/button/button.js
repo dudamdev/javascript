ac4032ca98d04e860490d22cf3b8fa6e6a1a84e0
dudamdev <111284160+dudamdev@users.noreply.github.com>
2023-02-14 07:23:26 -0300

    small update

--- freecodecamp-JS/javascript-programming-full-course/button/button.js
+++ freecodecamp-JS/javascript-programming-full-course/button/button.js
@@ -1,6 +1,12 @@
-const recipient = "James"
-const email = `
-        Hey ${recipient}! How is it going? Cheers Duda
-`
+// const recipient = "James"
+// const email = `
+//         Hey ${recipient}! How is it going? Cheers Duda
+// `
 
+// console.log(email)
+
+const recipient = "James"
+let sender = "Duda"
+
+const email = `Hey ${recipient}! How is it going? Cheers ${sender}`
 console.log(email)
