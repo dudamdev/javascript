2c1b7f3a01bcf6cef84b6336ca5f43fe1aad3a02
Eduarda Matos <duda.mdev@gmail.com>
2023-02-18 18:01:02 -0300

    Finish Course/Deploy Extention

--- freecodecamp-JS/javascript-programming-full-course/button/button.js
+++ freecodecamp-JS/javascript-programming-full-course/button/button.js
@@ -1,16 +1,5 @@
-// const recipient = "James"
-// const email = `
-//         Hey ${recipient}! How is it going? Cheers Duda
-// `
+function getFirst(arr) {
+    return arr[0]
+}
 
-// console.log(email)
-
-const recipient = "James"
-let sender = "Duda"
-
-const email = `
-Hey ${recipient}! 
-How is it going? Cheers ${sender}
-`
-
-console.log(email)
+console.log(getFirst([1,2,3]))
--- freecodecamp-JS/javascript-programming-full-course/chrome-extension/extention.js
+++ freecodecamp-JS/javascript-programming-full-course/chrome-extension/extention.js
@@ -1,30 +1,31 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
 const ulEl = document.getElementById("ul-el")
-
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    render(myLeads)
 }
 
-inputBtn.addEventListener("click", function () {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    renderLeads()
-    console.log(localStorage.getItem("myLeads"))
+tabBtn.addEventListener("click", function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
 })
 
-function renderLeads() {
+function render(leads) {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
         <li>
-            <a target='_blank' href='${myLeads[i]}'>
-            ${myLeads[i]}
+            <a target='_blank' href='${leads[i]}'>
+            ${leads[i]}
             </a>
         </li>
     `
@@ -32,4 +33,16 @@ function renderLeads() {
     ulEl.innerHTML = listItems
 }
 
-// JSON - JavaScript Object Notation
+deleteBtn.addEventListener("dblclick", function () {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+inputBtn.addEventListener("click", function () {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+    console.log(localStorage.getItem("myLeads"))
+})
