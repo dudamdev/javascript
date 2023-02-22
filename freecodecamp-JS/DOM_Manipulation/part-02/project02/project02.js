8ac77877ca5d93cfa08559017b7a61c8e083be48
Eduarda Matos <111284160+dudamdev@users.noreply.github.com>
2023-02-22 00:32:09 -0300

    Update project02.js

--- freecodecamp-JS/DOM_Manipulation/part-02/project02/project02.js
+++ freecodecamp-JS/DOM_Manipulation/part-02/project02/project02.js
@@ -10,3 +10,13 @@ openBtn.addEventListener("click", function () {
     
     modalContainer.style.display = "block"
 })
+
+closeBtn.addEventListener("click", function () {
+    modalContainer.style.display = "none"
+});
+
+window.addEventListener("click", function (e) {
+    if (e.target === modalContainer) {
+        modalContainer.style.display = "none"
+    }
+})
